097160dd9286ac0ed8fc1337d3d1006b6142729b
Robby_Malunga_Jr <46316955+iamrobbiejr@users.noreply.github.com>
2023-10-17 16:55:05 +0200

    Update index.blade.php

--- resources/views/library/index.blade.php
+++ resources/views/library/index.blade.php
@@ -5,15 +5,13 @@
         <h2 class="h2 page-title p-2">Class: </h2>
         <div class="row">
             @foreach($books as $book)
-                <div class="col-md-3 p-2">
+                <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 p-2">
                     <div class="card">
                         <div class="card-header bg-dark-grey">
                             <h3>{{ $book->name }}</h3>
                         </div>
                         <div class="card-body">
-                            <p>
-                                <img width="261" height="300" src="{{ asset('uploads/books/thumbnails/'.$book->thumbnail) }}" alt="{{ $book->name }}">
-                            </p>
+                            <img width="261" height="300" src="{{ asset('uploads/books/thumbnails/'.$book->thumbnail) }}" alt="{{ $book->name }}">
                         </div>
                         <div class="card-footer ">
                             <button class="btn btn-sm btn-success rounded-pill">Read</button>
