2baa98d24f488ebd91836f44d39a9a3cd9e877d7
Robby_Malunga_Jr <46316955+iamrobbiejr@users.noreply.github.com>
2023-10-24 08:37:05 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,57 @@
 @extends('layouts.guest')
 
 @section('content')
+    <style>
+        .fly-in {
+            display: none;
+            position: relative;
+            left: -100px;
+            opacity: 0;
+            transition: left 1s ease-in-out, opacity 1s ease-in-out;
+        }
 
-    <div class="row flex-grow">
+        .fly-in.show {
+            display: block;
+            left: 0;
+            opacity: 1;
+        }
+        .loading-spinner {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        .spinner {
+            width: 200px;
+            height: 200px;
+            border-radius: 50%;
+            border: 8px solid #f3f3f3;
+            border-top: 8px solid #3498db;
+            animation: spin 4s linear infinite;
+        }
+
+        .loading-text {
+            margin-top: 10px;
+            font-size: 18px;
+        }
+
+        @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+        }
+    </style>
+    <div class="row flex-grow" id="loading-spinner">
+        <div class="col-lg-4 mx-auto">
+                <div class="loading-spinner">
+                    <div class="spinner"></div>
+                    <div class="brand-logo loading-text">powered by <span class="font-weight-bold" style="font-size: 28px;">Mahlamvana</span></div>
+                </div>
+        </div>
+    </div>
+
+    <div id="main" class="fly-in row flex-grow">
                 <div class="col-lg-4 mx-auto">
                     <div class="auth-form-light rounded text-left p-5">
                         <div class="brand-logo">
@@ -52,5 +101,12 @@
                     </div>
                 </div>
             </div>
-
+    <script>
+        // Hide the spinner after 5 seconds
+        setTimeout(function() {
+            document.getElementById('loading-spinner').style.display = 'none';
+            document.getElementById('main').classList.add('show');
+            // document.getElementById('main').style.display = 'block'
+        }, 4000);
+    </script>
 @endsection
